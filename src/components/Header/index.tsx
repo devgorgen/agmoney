9d08ad38405a99253c06d160612bc20f89780a61
Augusto Gorgen <dev.gorgen@gmail.com>
2021-03-29 08:23:13 -0300

    Adding initial structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content, Button } from './styles'
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+    
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <Button type="button" onClick={onOpenNewTransactionModal}>Nova transação</Button>         
+            </Content>
+        </Container>
+    )
+}
