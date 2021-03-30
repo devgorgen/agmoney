db2edb0e91bf43ddfb86e51b68387e548f9f27e1
Augusto Gorgen <dev.gorgen@gmail.com>
2021-03-30 08:06:21 -0300

    Adding context to provider to share the transactions state by the components

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from './styles';
 
 export function Summary() {
+    const transitions = useContext(TransactionsContext);
+
+    console.log(transitions);
+
     return (
         <Container>
             <div>
