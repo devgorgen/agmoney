75a63739006dec6809161d1bcebfeb67b2701b1e
Augusto Gorgen <dev.gorgen@gmail.com>
2021-03-30 08:27:03 -0300

    Creating method createTransaction inside transactionsContext
    * Creating method to add transaction inside transactions context to use in components.
    * using this method (createTransaction) to create a new transaction.
    * Using Omit exclude some properties of an interface that already exist

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -15,7 +15,22 @@ interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+/* 
+    Mesma ideia de interface para tipar os parametros
+    porém, usando o Omit posso passar uma interface ou tipo já existente
+    e apenas omitir o que não desejo, ao invés de declarar todas as propriedades novamente.
+    também poderia usar o Pick para escolher quais propriedades eu quero incluir
+*/
+type TransactionInput = Omit<Transaction, 'id' | 'createAt'>;
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData //forçando o tipo apenas para parar o alert de erro, não vai afetar a execução
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransaction] = useState<Transaction[]>([]);
@@ -25,9 +40,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
         .then(response => setTransaction(response.data.transactions))
     }, []);
 
+    function createTransaction(transaction: TransactionInput) {
+      
+        api.post('/transactions', transaction);
+    }
     
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
